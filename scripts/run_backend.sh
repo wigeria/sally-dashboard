bc9bb9b0a38fd0053a34243c2a7a6c63c1ed0d83
wigeria <wigeriaaeriag@gmail.com>
2021-09-17 10:25:12 +0530

    Worker-autorestarts using a while loop (#15)

--- scripts/run_backend.sh
+++ scripts/run_backend.sh
@@ -5,7 +5,10 @@ cd /code/
 pip install -r /code/requirements.txt
 
 # Starting the tasks service
-/scripts/spawn_worker.sh tasks.jobs &
+(while true; do
+    # This line halts execution inside the loop until a crash/exit
+    /scripts/spawn_worker.sh tasks.jobs
+done) &
 
 # Starting the backend service
 python -m backend.__init__
