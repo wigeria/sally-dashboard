a5cbbb583c1b7c244e4db57f11302d175fa60aaf
Abhishek Verma <wigeriaaeriag@gmail.com>
2024-05-10 10:43:54 +0000

    Dramatiq rate limit

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -5,8 +5,15 @@
 from backend import settings
 import dramatiq
 from dramatiq.brokers.redis import RedisBroker
+from dramatiq.rate_limits import ConcurrentRateLimiter
+from dramatiq.rate_limits.backends import RedisBackend
 import os
 
 
 broker = RedisBroker(**settings.REDIS_CONF)
 dramatiq.set_broker(broker)
+
+backend = RedisBackend(**settings.REDIS_CONF)
+# Hard coded to 4 concurrent tasks at a time
+DISTRIBUTED_MUTEX = ConcurrentRateLimiter(
+    backend, "distributed-mutex", limit=4)
